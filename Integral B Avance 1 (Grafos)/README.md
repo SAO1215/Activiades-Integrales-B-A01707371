5ede8cb299dba39aaad5e995d22d36fe10bc72ec
SAO1215 <70983072+SAO1215@users.noreply.github.com>
2021-11-17 23:30:40 -0600

    Add files via upload

---
+++ Integral B Avance 1 (Grafos)/README.md
@@ -0,0 +1,41 @@
+# ¿TE PERDIS-TEC? Integral B Avance 1 (Grafos)
+Olivia Araceli Morales Quezada (A01707371)
+
+La finalidad de este proyecto es implementar grafos por medio de un programa que mapea el area de profesional del TEC y crea un grafo con el que podemos ver los sitios mas cercanos a nuesta ubicación y en caso de querer llegar a un lugar en especifico la ruta más optima. 
+
+Imagenes del mapa del TEC y el grafo generado:
+
+![Alt text]()
+![Alt text]()
+
+## Competencias a evaluar
+
+###SICT0301B: Evalúa los componentes
+
+**Presenta Casos de Prueba correctos y completos para todas las funciones y procedimientos del programa**
+
+Aun no se hace, pero se planea hacer.
+
+### SICT0302B: Toma decisiones
+
+**Usa grafos para hacer analisis de información** 
+
+En el archivo _mapa.txt_ se encuentran las relaciones adyacentes de los lugares del TEC, estos van a guardarse en una lista de adyacencia ya que su proposito es mostrar rutas generadas a partir de las busquedas del grafo con esta información. 
+
+**Usa un algoritmo de búsqueda en grafos adecuado para resolver un problema**
+
+Para esta situación el algoritmo de búsqueda seleccionado fue un BFS, a diferencia del DFS este te muestra el camino más corto, y es el adecuado para mostrarte la ruta más óptima para llegar a donde quieras. 
+
+### SICT0303B: Implementa acciones científicas (Ya calificada en Integral A)
+
+**Implementa mecanismos de lectura de archivos correctos y útiles dentro de un programa.** 
+
+Se leen los datos del archivo "mapa.txt", este proceso se encuentra en la funcion _cargarLista()_, implementada en la linea 42 de _main.cpp_.
+
+## Consideraciones
+- Solo ingresar los numeros que corresponden a cada lugar, no el nombre.
+- Puedo contar con una mano las veces que he pisado el TEC, y casi me perdí con el guia (@AudibleP) es posible que existan algunos errores o carencias, es solo un ejemplo que se me ocurrio para implementar grafos.
+
+**Referencias**
+
+- Pagina para generar el grapho: https://csacademy.com/app/graph_editor/ 
---
+++ Integral B Avance 1 (Grafos)/graph.h
@@ -0,0 +1,193 @@
+/*
+ *
+ * Programación de estructuras de datos y algoritmos fundamentales (TC1031.2)
+ * Formativa 4.1 - Grafo como matriz de Adyacencia y recorridos (DFS BFS)
+ * Olivia Araceli Morales Quezada
+ * A01707371
+ *
+*/
+
+#ifndef Graph_H_
+#define Graph_H_
+
+#include <string>
+#include <stdio.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+
+class Graph {
+private:
+		int arcosLista;
+		//int edgesMat;
+		int nodos;
+    vector<int> *adjLista;
+		//int *adjMatrix;
+		//vector<Type> *vect = new vector<Type>
+
+public:
+		void loadGraphMat(string, int, int);
+		Graph(int);
+		Graph();
+		void addEdgeAdjMatrix(int, int);
+    void añadirLista(int, int);
+		string printAdjMat();
+    string printadjLista();
+    string cerca(int);
+		bool contains(list<int>, int);
+
+    //Metodos tarea
+    void cargarLista(string, int, int);
+    string DFS(int, int);
+    string BFS(int, int);
+
+    //Auxiliares
+    string DFS_help(int, int, stack<int>&, list<int>&, vector<vector<int>>&);
+    string BFS_help(int, int, queue<int>&, list<int>&, vector<vector<int>>&);
+    string print_ruta(vector<vector<int>>&, int, int);
+    string print_visitados(list<int>);
+    
+    
+};
+
+Graph::Graph() {
+	arcosLista = 0;
+}
+
+Graph::Graph(int n) {
+  nodos = n;
+	adjLista = new vector<int>[nodos];
+	arcosLista = 0;
+}
+
+
+bool Graph::contains(list<int> ls, int nodo){
+		list<int>::iterator it;
+		it = find(ls.begin(), ls.end(), nodo);
+		if(it != ls.end())
+			return true;
+		else
+			return false;
+}
+
+void Graph::añadirLista(int u, int v){
+	adjLista[v].push_back(u);
+	//adjLista[u].push_back(v);
+	arcosLista++;
+}
+
+void Graph::cargarLista(string nombre, int a, int b){
+	adjLista = new vector<int> [a];
+  nodos = a;
+
+	string line;
+	ifstream lee (nombre);
+  int u, v;
+		if (lee.is_open()){
+			while (getline(lee, line)){
+				u = stoi(line.substr(1));
+				v = stoi(line.substr(4));
+				añadirLista(u,v);
+			}
+			lee.close();
+		} else {
+			cout << "Unable to open file";
+		}
+}
+
+string Graph::printadjLista(){
+	  stringstream aux;
+    for (int i = 0; i < nodos; i++){
+		sort(adjLista[i].begin(),adjLista[i].end());
+	  }
+		for (int i = 0; i < nodos; i++){
+	        aux << "vertex "
+	             << i << " :";
+	        for (int j = 0; j < adjLista[i].size(); j++){
+							 aux << " " << adjLista[i][j];
+					}
+	        aux << " ";
+    }
+		return aux.str();
+
+}
+
+string Graph::cerca(int origen){
+  stringstream aux;
+  for (int j = 0; j < adjLista[origen].size(); j++){
+							 aux << " " << adjLista[origen][j];
+					}
+  return aux.str();
+}
+
+string Graph::BFS(int origen, int meta){
+  stringstream aux;
+	queue <int> qu;
+	list <int> visitados;
+  vector <vector<int>> ruta(nodos, vector<int>(0));
+	qu.push(origen);
+  BFS_help(origen, meta, qu, visitados, ruta);
+  //aux << print_visitados(visitados) 
+  aux << print_ruta(ruta, origen, meta);
+  return aux.str();
+}
+
+string Graph::BFS_help(int origen, int meta, queue<int> &qu, list<int> &visitados, vector<vector<int>> &ruta){
+  stringstream aux;
+	if(origen == meta){
+			print_visitados(visitados);
+		} else if(qu.empty()){
+			cout << " nodo not found";
+		}	else {
+			origen = qu.front();
+			qu.pop();
+			visitados.push_back(origen);
+			for(int  i = 0; i < adjLista[origen].size(); i++)
+				if(!contains(visitados, adjLista[origen][i])){
+					qu.push(adjLista[origen][i]);
+					ruta[adjLista[origen][i]].push_back(origen);
+				}
+			BFS_help(origen, meta, qu, visitados, ruta);
+		}
+  return aux.str();
+}
+
+string Graph::print_ruta(vector<vector <int>> &ruta, int origen, int meta){
+  stringstream aux;
+	int nodo =  ruta[meta][0];
+	stack<int> reverse;
+	reverse.push(meta);
+	aux << "ruta:";
+	while (nodo != origen) {
+		reverse.push(nodo);
+    nodo = ruta[nodo][0];
+  }
+	reverse.push(origen);
+	while (!reverse.empty()) {
+		aux << " " << reverse.top();
+		reverse.pop();
+  }
+  //aux << endl;
+  return aux.str();
+  
+}
+
+string Graph::print_visitados(list<int> visitados){
+	stringstream aux;
+  aux << "visitados: ";
+	while (!visitados.empty()){
+    aux << visitados.front() << " ";
+    visitados.pop_front();
+  }
+  //aux << endl;
+  return aux.str();
+}
+#endif /* Graph_H_ */
